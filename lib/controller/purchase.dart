bfdcd8d93a12b562723a0f5e7304aa2ce3d95708
rizalblog <rizaldimohammad2002@gmail.com>
2022-04-21 16:26:11 +0700

    minggu_9

---
+++ lib/controller/purchase.dart
@@ -0,0 +1,34 @@
+import 'package:acara_29/model/product.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import '/model/product.dart';
+
+class Purchase extends GetxController {
+  var products = <Product>[].obs;
+  @override
+  void onInit(){
+    fetchProduct();
+    super.onInit();
+  }
+
+  void fetchProduct() async {
+    await Future.delayed(Duration(seconds: 1));
+  
+    var serverResponse = [
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+      Product(1, "Demo Product", "aby",
+          "This is a Product that we are going to show by getX", 300.0),
+     ];
+
+    products.value = serverResponse;
+  }
+}
---
+++ lib/model/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  final int id;
+  final String productName;
+  final String productImage;
+  final String ProductDescription;
+  final double price;
+
+  Product(this.id, this.productName, this.productImage, this.ProductDescription, this.price); 
+}
